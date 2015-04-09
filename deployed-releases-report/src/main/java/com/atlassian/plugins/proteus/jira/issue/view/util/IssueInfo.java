c916db52f6054285c843107a090bf0d9b8d3ffa5
HongFa Tang <hongfa.tang@ericsson.com>
2015-04-09 16:22:01 +1000

    fix bug -- if issue hasn't deploy to any env. it shouldn't be listed

--- deployed-releases-report/src/main/java/com/atlassian/plugins/proteus/jira/issue/view/util/IssueInfo.java
+++ deployed-releases-report/src/main/java/com/atlassian/plugins/proteus/jira/issue/view/util/IssueInfo.java
@@ -6,117 +6,136 @@ import java.util.List;
 import java.util.Map.Entry;
 import java.util.Set;
 
+/**
+ * The purpose of this class is to record issue info
+ */
 public class IssueInfo implements Comparable<IssueInfo> {
-	
-	private Long issueNo;
-	private String issueKey;
-	private String issueTitle;
-	
-	/**
-	 * Right now, we have assumed for each of the JIRA issue, we will only have one component & one version.
-	 * It can be easily extended to support multi-components and version later on if needed.
-	 */
-	private String deployedComponent;
-	private String deployedVersion;
-	
-	private List<DeploymentActivityRecord> activityRcd; 
-	
-	public Long getIssueNo() {
-		return issueNo;
-	}
-	
-	public IssueInfo setIssueNo(Long issueNo) {
-		this.issueNo = issueNo;
-		return this;
-	}
-	public String getIssueTitle() {
-		return issueTitle;
-	}
-	
-	public IssueInfo setIssueTitle(String issueTitle) {
-		this.issueTitle = issueTitle;
-		return this;
-	}
-	
-	public String getIssueKey() {
-		return issueKey;
-	}
 
-	public IssueInfo setIssueKey(String issueKey) {
-		this.issueKey = issueKey;
-		return this;
-	}
-	
-	public List<DeploymentActivityRecord> getActivityRcd() {
-		return activityRcd;
-	}
+    private Long issueNo;
+    private String issueKey;
+    private String issueTitle;
 
-	public IssueInfo setActivityRcd(List<DeploymentActivityRecord> activityRcd) {
-		this.activityRcd = activityRcd;
-		Collections.sort(this.activityRcd);
-		
-		DeploymentActivityRecord lastDep = this.getLastDeploymentToEnv(null);
-		Entry<String, String> oneComp = lastDep.getComponentVersionMap().entrySet().iterator().next();
-		this.deployedComponent = oneComp.getKey();
-		this.deployedVersion = oneComp.getValue();
-		
-		return this;
-	}
+    /**
+     * Right now, we have assumed for each of the JIRA issue, we will
+     * only have one component & one version. It can be easily
+     * extended to support multi-components and version later on if
+     * needed.
+     */
+    private String deployedComponent;
+    private String deployedVersion;
 
-	public String getDeployedComponent() {
-		return deployedComponent;
-	}
+    private List<DeploymentActivityRecord> activityRcd;
 
-	public String getDeployedVersion() {
-		return deployedVersion;
-	}
+    /**
+     * @return issueNo
+     */
+    public Long getIssueNo() {
+        return issueNo;
+    }
 
-	public String toString() {
-		return this.issueTitle;
-	}
+    public IssueInfo setIssueNo(Long issueNo) {
+        this.issueNo = issueNo;
+        return this;
+    }
 
-	@Override
-	public int compareTo(IssueInfo anotherIssue) {
-		return (int)(anotherIssue.getIssueNo() - this.getIssueNo());
-	}
-	
-	/**
-	 * Return the last deployment activity for an specified environment.
-	 * @param environment The environment name. When null, it means any environment.
-	 * @return
-	 */
-	public DeploymentActivityRecord getLastDeploymentToEnv(String environment) {
-		if(activityRcd == null) {
-			return null;
-		}
-		
-		//If environment is not specified, then just return the last deployment to any env.
-		if(environment == null) {
-			for(int i = 0; i < activityRcd.size(); i ++) {
-				DeploymentActivityRecord activity = activityRcd.get(i);
-				if(activity.getAction().equalsIgnoreCase(DeploymentActivityRecord.ACTION_TYPE_START_DEPLOY)) {
-					return activity;
-				}
-			}
-		}
-		
-		//This is the normal use-case, which is return the last deployment activity for specified environment
-		for(int i = 0; i < activityRcd.size(); i ++) {
-			DeploymentActivityRecord activity = activityRcd.get(i);
-			if(activity.getEnvironment().equalsIgnoreCase(environment) && activity.getAction().equalsIgnoreCase(DeploymentActivityRecord.ACTION_TYPE_START_DEPLOY) ) {
-				return activity;
-			}
-		}
-		
-		return null;
-	}
-	
-	public Set<String> getDeployedEnvironments() {
-		Set<String> envHash = new HashSet<String>();
-		for(int i = 0; i < activityRcd.size(); i ++) {
-			envHash.add(activityRcd.get(i).getEnvironment());
-		}
-		return envHash;
-	}
-	
+    public String getIssueTitle() {
+        return issueTitle;
+    }
+
+    public IssueInfo setIssueTitle(String issueTitle) {
+        this.issueTitle = issueTitle;
+        return this;
+    }
+
+    public String getIssueKey() {
+        return issueKey;
+    }
+
+    public IssueInfo setIssueKey(String issueKey) {
+        this.issueKey = issueKey;
+        return this;
+    }
+
+    public List<DeploymentActivityRecord> getActivityRcd() {
+        return activityRcd;
+    }
+
+    public IssueInfo setActivityRcd(List<DeploymentActivityRecord> activityRcd) {
+        this.activityRcd = activityRcd;
+        Collections.sort(this.activityRcd);
+
+        DeploymentActivityRecord lastDep = this.getLastDeploymentToEnv(null);
+        if (lastDep != null) {
+            Entry<String, String> oneComp = lastDep.getComponentVersionMap().entrySet().iterator().next();
+            this.deployedComponent = oneComp.getKey();
+            this.deployedVersion = oneComp.getValue();
+        }
+
+        return this;
+    }
+
+    public String getDeployedComponent() {
+        return deployedComponent;
+    }
+
+    public String getDeployedVersion() {
+        return deployedVersion;
+    }
+
+    @Override
+    public String toString() {
+        return this.issueTitle;
+    }
+
+    @Override
+    public int compareTo(IssueInfo anotherIssue) {
+        return (int) (anotherIssue.getIssueNo() - this.getIssueNo());
+    }
+
+    /**
+     * Return the last deployment activity for an specified
+     * environment.
+     * 
+     * @param environment The environment name. When null, it means
+     *            any environment.
+     * @return
+     */
+    public DeploymentActivityRecord getLastDeploymentToEnv(String environment) {
+        if (activityRcd == null) {
+            return null;
+        }
+
+        //If environment is not specified, then just return the last deployment to any env.
+        if (environment == null) {
+            for (int i = 0; i < activityRcd.size(); i++) {
+                DeploymentActivityRecord activity = activityRcd.get(i);
+                if (activity.getAction().equalsIgnoreCase(DeploymentActivityRecord.ACTION_TYPE_START_DEPLOY)) {
+                    return activity;
+                }
+            }
+        }
+
+        //This is the normal use-case, which is return the last deployment activity for specified environment
+        for (int i = 0; i < activityRcd.size(); i++) {
+            DeploymentActivityRecord activity = activityRcd.get(i);
+            if (activity.getEnvironment().equalsIgnoreCase(environment)
+                    && activity.getAction().equalsIgnoreCase(DeploymentActivityRecord.ACTION_TYPE_START_DEPLOY)) {
+                return activity;
+            }
+        }
+
+        return null;
+    }
+
+    public Set<String> getDeployedEnvironments() {
+        Set<String> envHash = new HashSet<String>();
+        for (int i = 0; i < activityRcd.size(); i++) {
+            envHash.add(activityRcd.get(i).getEnvironment());
+        }
+        return envHash;
+    }
+
+    public boolean isEverDeployed() {
+        return (activityRcd != null) && (activityRcd.size() > 0);
+    }
 }
